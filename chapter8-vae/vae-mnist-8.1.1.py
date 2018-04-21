f2e75c95f5666564bf0c59498c40c5d3fb1bd615
Rowel Atienza <rowel.atienza@gmail.com>
2018-04-21 23:14:33 +0800

    vae fixes

--- chapter8-vae/vae-mnist-8.1.1.py
+++ chapter8-vae/vae-mnist-8.1.1.py
@@ -1,9 +1,10 @@
-'''Example of VAE on MNIST dataset
+'''Example of VAE on MNIST dataset using CNN
 
-This autoencoder has modular design. The encoder, decoder and autoencoder
-are 3 models that share weights. For example, after training the
-autoencoder, the encoder can be used to  generate latent vectors
-of input data for low-dim visualization like PCA or TSNE.
+This VAE has a modular design. The encoder, decoder and vae
+are 3 models that share weights. After training vae,
+the encoder can be used to  generate latent vectors.
+The decoder can be used to generate MNIST digits by sampling the 
+latent vector from a gaussian dist with mean=0 and std=1
 '''
 
 from __future__ import absolute_import
@@ -34,7 +35,7 @@ x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-# Network parameters
+# network parameters
 input_shape = (image_size, image_size, 1)
 batch_size = 128
 kernel_size = 3
@@ -54,46 +55,44 @@ def sampling(args):
     return z_mean + K.std(K.exp(z_log_var)) * epsilon
 
 
-# Build the VAE model
-# First build the encoder model
+# VAE model = encoder + decoder
+# build encoder model
 inputs = Input(shape=input_shape, name='encoder_input')
 x = inputs
-# Stack of BN-ReLU-Conv2D-MaxPooling2D blocks
 for i in range(2):
-    # x = BatchNormalization()(x)
-    x = Activation('relu')(x)
     filters *= 2
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
     x = Conv2D(filters=filters,
                kernel_size=kernel_size,
                padding='same')(x)
     x = MaxPooling2D()(x)
 
-# Shape info needed to build decoder model
+# shape info needed to build decoder model
 shape = K.int_shape(x)
 
-
-# Generate latent vector (Q(z|X))
+# generate latent vector Q(z|X)
 x = Flatten()(x)
 x = Dense(16, activation='relu')(x)
 z_mean = Dense(latent_dim, name='z_mean')(x)
 z_log_var = Dense(latent_dim, name='z_log_var')(x)
 
+# use reparameterization trick to push the sampling out as input
 # note that "output_shape" isn't necessary with the TensorFlow backend
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 
-# Instantiate encoder model
+# instantiate encoder model
 encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
 encoder.summary()
 plot_model(encoder, to_file='encoder.png', show_shapes=True)
 
-# Build the decoder model
+# build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
-x = Dense(shape[1]*shape[2]*shape[3])(latent_inputs)
+x = Dense(shape[1]*shape[2]*shape[3], activation='relu')(latent_inputs)
 x = Reshape((shape[1], shape[2], shape[3]))(x)
 
-# Stack of BN-ReLU-Transposed Conv2D-UpSampling2D blocks
 for i in range(2):
-    # x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Conv2DTranspose(filters=filters,
                         kernel_size=kernel_size,
@@ -107,31 +106,23 @@ x = Conv2DTranspose(filters=1,
 
 outputs = Activation('sigmoid', name='decoder_output')(x)
 
-# Instantiate decoder model
+# instantiate decoder model
 decoder = Model(latent_inputs, outputs, name='decoder')
 decoder.summary()
 plot_model(decoder, to_file='decoder.png', show_shapes=True)
 
-# vae = Model(inputs, outputs, name='vae')
+# instantiate vae model
 outputs = decoder(encoder(inputs)[2])
 vae = Model(inputs, outputs, name='vae')
 
-# Compute VAE loss
-xent_loss = losses.mse(
-            K.flatten(inputs),
-            K.flatten(outputs))
-xent_loss *= image_size * image_size 
-
-# print("xent_loss shape: ", K.int_shape(xent_loss))
-# print("x shape: ", K.int_shape(x))
-# print("x decoded shape: ", K.int_shape(x_decoded_mean))
-kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-vae_loss = K.mean(xent_loss + kl_loss)
-
+# VAE loss = mse_loss + kl_loss
+mse_loss = losses.mse(K.flatten(inputs), K.flatten(outputs))
+mse_loss *= image_size * image_size 
+kl_loss = K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+kl_loss *= -0.5
+vae_loss = K.mean(mse_loss + kl_loss)
 vae.add_loss(vae_loss)
 vae.compile(optimizer='rmsprop')
-
-
 vae.summary()
 plot_model(vae, to_file='vae.png', show_shapes=True)
 
@@ -150,11 +141,13 @@ plt.colorbar()
 plt.show()
 
 # display a 2D manifold of the digits
-n = 30 # figure with 15x15 digits
+n = 20 # figure with 15x15 digits
 digit_size = 28
 figure = np.zeros((digit_size * n, digit_size * n))
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian
+# linearly spaced coordinates on the unit square were 
+# transformed through the inverse CDF (ppf) of the Gaussian
+# to produce values of the latent variables z, 
+# since the prior of the latent space is Gaussian
 grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
 grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
