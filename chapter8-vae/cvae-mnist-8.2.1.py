2417fb81de682fc441bfa1c839867677087dd271
Rowel Atienza <rowel.atienza@gmail.com>
2018-04-22 18:53:20 +0800

    cvae commit

--- chapter8-vae/cvae-mnist-8.2.1.py
+++ chapter8-vae/cvae-mnist-8.2.1.py
@@ -1,4 +1,4 @@
-'''Example of VAE on MNIST dataset using CNN
+'''Example of CVAE on MNIST dataset using CNN
 
 This VAE has a modular design. The encoder, decoder and vae
 are 3 models that share weights. After training vae,
@@ -20,12 +20,13 @@ from keras.datasets import mnist
 from keras import losses
 from keras.utils import plot_model
 from keras import backend as K
+from keras.utils import to_categorical
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 import argparse
-
+import os
 
 # reparameterization trick
 # instead of sampling from Q(z|X), sample eps = N(0,I)
@@ -41,18 +42,29 @@ def sampling(args):
 
 # plot 2-dim mean values of Q(z|X) using labels as color gradient
 # then, plot MNIST digits as function of 2-dim latent vector
-def plot_results(models, data):
+def plot_results(models,
+                 data,
+                 y_label,
+                 batch_size=128,
+                 model_name="cvae_mnist"):
+
     encoder, decoder = models
     x_test, y_test = data
+    os.makedirs(model_name, exist_ok=True)
+
+    filename = os.path.join(model_name, "_mean_.png")
     # display a 2D plot of the digit classes in the latent space
-    x_test_encoded, _, _ = encoder.predict(x_test, batch_size=batch_size)
+    z_mean, _, _ = encoder.predict([x_test, to_categorical(y_test)],
+                                   batch_size=batch_size)
     plt.figure(figsize=(6, 6))
-    plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
+    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
     plt.colorbar()
+    plt.savefig(filename)
     plt.show()
 
+    filename = os.path.join(model_name, "_%05d.png" % np.argmax(y_label))
     # display a 2D manifold of the digits
-    n = 20 # figure with 20x20 digits
+    n = 10 # figure with 10x10 digits
     digit_size = 28
     figure = np.zeros((digit_size * n, digit_size * n))
     # linearly spaced coordinates on the unit square were 
@@ -65,15 +77,16 @@ def plot_results(models, data):
     for i, yi in enumerate(grid_x):
         for j, xi in enumerate(grid_y):
             z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
+            x_decoded = decoder.predict([z_sample, y_label])
             digit = x_decoded[0].reshape(digit_size, digit_size)
             figure[i * digit_size: (i + 1) * digit_size,
                    j * digit_size: (j + 1) * digit_size] = digit
 
     plt.figure(figsize=(10, 10))
     plt.imshow(figure, cmap='Greys_r')
+    plt.savefig(filename)
     plt.show()
-   
+    
 
 # MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -84,18 +97,25 @@ x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
+# compute the number of labels
+num_labels = len(np.unique(y_train))
+
 # network parameters
 input_shape = (image_size, image_size, 1)
+label_shape = (num_labels, )
 batch_size = 128
 kernel_size = 3
 filters = 16
 latent_dim = 2
-epochs = 30
+epochs = 10
 
 # VAE model = encoder + decoder
 # build encoder model
 inputs = Input(shape=input_shape, name='encoder_input')
-x = inputs
+y_labels = Input(shape=label_shape, name='class_labels')
+x = Dense(image_size * image_size)(y_labels)
+x = Reshape((image_size, image_size, 1))(x)
+x = keras.layers.concatenate([inputs, x])
 for i in range(2):
     filters *= 2
     x = Conv2D(filters=filters,
@@ -118,13 +138,14 @@ z_log_var = Dense(latent_dim, name='z_log_var')(x)
 z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
 
 # instantiate encoder model
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+encoder = Model([inputs, y_labels], [z_mean, z_log_var, z], name='encoder')
 encoder.summary()
 plot_model(encoder, to_file='encoder.png', show_shapes=True)
 
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
-x = Dense(shape[1]*shape[2]*shape[3], activation='relu')(latent_inputs)
+x = keras.layers.concatenate([latent_inputs, y_labels])
+x = Dense(shape[1]*shape[2]*shape[3], activation='relu')(x)
 x = Reshape((shape[1], shape[2], shape[3]))(x)
 
 for i in range(2):
@@ -142,13 +163,13 @@ x = Conv2DTranspose(filters=1,
 outputs = Activation('sigmoid', name='decoder_output')(x)
 
 # instantiate decoder model
-decoder = Model(latent_inputs, outputs, name='decoder')
+decoder = Model([latent_inputs, y_labels], outputs, name='decoder')
 decoder.summary()
 plot_model(decoder, to_file='decoder.png', show_shapes=True)
 
 # instantiate vae model
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae')
+outputs = decoder([encoder([inputs, y_labels])[2], y_labels])
+cvae = Model([inputs, y_labels], outputs, name='cvae')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -156,33 +177,45 @@ if __name__ == '__main__':
     parser.add_argument("-w", "--weights", help=help_)
     help_ = "Use mse loss instead of binary cross entropy (default)"
     parser.add_argument("-m", "--mse", help=help_, action='store_true')
+    help_ = "Specify a specific digit to generate"
+    parser.add_argument("-d", "--digit", type=int, help=help_)
     args = parser.parse_args()
     models = (encoder, decoder)
     data = (x_test, y_test)
+
     # VAE loss = mse_loss or xent_loss + kl_loss
     if args.mse:
         decoder_loss = losses.mse(K.flatten(inputs), K.flatten(outputs))
     else:
         decoder_loss = losses.binary_crossentropy(K.flatten(inputs),
-                                                      K.flatten(outputs))
+                                                  K.flatten(outputs))
     decoder_loss *= image_size * image_size 
     kl_loss = K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var),
                     axis=-1)
     kl_loss *= -0.5
-    vae_loss = K.mean(decoder_loss + kl_loss)
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='rmsprop')
-    vae.summary()
-    plot_model(vae, to_file='vae.png', show_shapes=True)
+    cvae_loss = K.mean(decoder_loss + kl_loss)
+    cvae.add_loss(cvae_loss)
+    cvae.compile(optimizer='rmsprop')
+    cvae.summary()
+    plot_model(cvae, to_file='cvae.png', show_shapes=True)
 
     if args.weights:
-        vae = vae.load_weights(args.weights)
+        cvae = cvae.load_weights(args.weights)
     else:
         # train the autoencoder
-        vae.fit(x_train,
-                epochs=epochs,
-                batch_size=batch_size,
-                validation_data=(x_test, None))
-        vae.save_weights('vae_mnist.h5')
+        cvae.fit([x_train, to_categorical(y_train)],
+                 epochs=epochs,
+                 batch_size=batch_size,
+                 validation_data=([x_test, to_categorical(y_test)], None))
+        cvae.save_weights('cvae_mnist.h5')
 
-    plot_results(models, data)
+    if args.digit:
+        digit = [args.digit]
+    else:
+        digit = np.random.randint(0, 10, 1) 
+
+    print(digit)
+    y_label = np.eye(num_labels)[digit]
+    print(y_label)
+
+    plot_results(models, data, y_label)
