bce838d8a2719976a5e64968c1d914bbeaff3259
Rowel Atienza <rowel.atienza@gmail.com>
2018-04-26 12:10:56 +0800

    bug fixes on plots

--- chapter8-vae/vae-mlp-mnist-8.1.1.py
+++ chapter8-vae/vae-mlp-mnist-8.1.1.py
@@ -75,25 +75,26 @@ def plot_results(models,
     # display a 2D plot of the digit classes in the latent space
     z_mean, _, _ = encoder.predict(x_test,
                                    batch_size=batch_size)
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(12, 10))
     plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
     plt.colorbar()
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
     plt.savefig(filename)
     plt.show()
 
     filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 2D manifold of the digits
-    n = 20  # figure with 20x20 digits
+    # display a 30x30 2D manifold of the digits
+    n = 30 
     digit_size = 28
     figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates on the unit square were
-    # transformed through the inverse CDF (ppf) of the Gaussian
-    # to produce values of the latent variables z,
-    # since the prior of the latent space is Gaussian
-    grid = norm.ppf(np.linspace(0.05, 0.95, n))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-4, 4, n)
+    grid_y = np.linspace(-4, 4, n)[::-1]
 
-    for i, yi in enumerate(grid):
-        for j, xi in enumerate(grid):
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
             z_sample = np.array([[xi, yi]])
             x_decoded = decoder.predict(z_sample)
             digit = x_decoded[0].reshape(digit_size, digit_size)
@@ -104,9 +105,10 @@ def plot_results(models,
     start_range = digit_size // 2
     end_range = n * digit_size + start_range + 1
     pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range = np.round(grid, 2)
-    plt.xticks(pixel_range, sample_range)
-    plt.yticks(pixel_range, sample_range)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
     plt.xlabel("z[0]")
     plt.ylabel("z[1]")
     plt.imshow(figure, cmap='Greys_r')
