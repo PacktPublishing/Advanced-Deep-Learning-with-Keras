4a29881731611d44604cfaeaf5244931c13f67ed
Rowel Atienza <rowel.atienza@gmail.com>
2018-05-25 06:29:11 +0800

    added comments

--- chapter2-deep-networks/cnn-functional-2.1.1.py
+++ chapter2-deep-networks/cnn-functional-2.1.1.py
@@ -1,43 +1,39 @@
 ''' Using Functional API to build CNN
 
-3-layer CNN for MNIST digits classification 
-First 2 layers - Conv2D-ReLU-MaxPool
-3rd layer - Conv2D-ReLU-Dropout
-4th layer - Dense(10)
-Output Activation - softmax
-Optimizer - Adam
-
-99.4% test accuracy
-
+~99.3% test accuracy
 '''
 
 import numpy as np
-from keras.layers import Activation, Dense, Dropout, Input
+from keras.layers import Dense, Dropout, Input
 from keras.layers import Conv2D, MaxPooling2D, Flatten
 from keras.models import Model
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
+
+# load MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# from sparse label to categorical
 num_labels = np.amax(y_train) + 1
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
+# reshape and normalize input images
 image_size = x_train.shape[1]
 x_train = np.reshape(x_train,[-1, image_size, image_size, 1])
 x_test = np.reshape(x_test,[-1, image_size, image_size, 1])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
+# network parameters
 input_shape = (image_size, image_size, 1)
-batch_size = 128
+batch_size = 32
 kernel_size = 3
-pool_size = 2
 filters = 64
 dropout = 0.3
 
-# Use Functional API to build CNN
+# use functional API to build cnn layers
 inputs = Input(shape=input_shape)
 y = Conv2D(filters=filters,
            kernel_size=kernel_size,
@@ -50,23 +46,29 @@ y = MaxPooling2D()(y)
 y = Conv2D(filters=filters,
            kernel_size=kernel_size,
            activation='relu')(y)
+# image to vector before connecting to dense layer
 y = Flatten()(y)
+# dropout regularization
 y = Dropout(dropout)(y)
-y = Dense(num_labels)(y)
-outputs = Activation('softmax')(y)
+outputs = Dense(num_labels, activation='softmax')(y)
 
-# Build the model by supplying inputs/outputs
+# build the model by supplying inputs/outputs
 model = Model(inputs=inputs, outputs=outputs)
+# network model in text
 model.summary()
 
+# classifier loss, Adam optimizer, classifier accuracy
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+
+# train the model with input images and labels
 model.fit(x_train,
           y_train,
           validation_data=(x_test, y_test),
           epochs=20,
           batch_size=batch_size)
 
+# model accuracy on test dataset
 score = model.evaluate(x_test, y_test, batch_size=batch_size)
 print("\nTest accuracy: %.1f%%" % (100.0 * score[1]))
--- chapter2-deep-networks/cnn-y-network-2.1.2.py
+++ chapter2-deep-networks/cnn-y-network-2.1.2.py
@@ -1,43 +1,46 @@
-'''
-Implementing a Y-Network using Functional API
+'''Implements a Y-Network using Functional API
 
-Project: https://github.com/roatienza/dl-keras
-Dependencies: keras 
-Usage: python3 <this file>
+~99.3% test accuracy
 '''
 
 import numpy as np
-import keras
-from keras.layers import Activation, Dense, Dropout, Input
+
+from keras.layers import Dense, Dropout, Input
 from keras.layers import Conv2D, MaxPooling2D, Flatten
 from keras.models import Model
+from keras.layers.merge import concatenate
 from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.utils import plot_model
 
+# load MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# from sparse label to categorical
 num_labels = np.amax(y_train) + 1
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
+# reshape and normalize input images
 image_size = x_train.shape[1]
 x_train = np.reshape(x_train,[-1, image_size, image_size, 1])
 x_test = np.reshape(x_test,[-1, image_size, image_size, 1])
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
+# network parameters
 input_shape = (image_size, image_size, 1)
-batch_size = 128
+batch_size = 32
 kernel_size = 3
-pool_size = 2
 dropout = 0.4
 n_filters = 32
 
-# Left branch of Y network
-inputs1 = Input(shape=input_shape)
-x = inputs1
+# left branch of Y network
+left_inputs = Input(shape=input_shape)
+x = left_inputs
 filters = n_filters
+# 3 layers of Conv2D-Dropout-MaxPooling2D
+# number of filters doubles after each layer (32-64-128)
 for i in range(3):
     x = Conv2D(filters=filters,
                kernel_size=kernel_size,
@@ -45,12 +48,14 @@ for i in range(3):
                activation='relu')(x)
     x = Dropout(dropout)(x)
     x = MaxPooling2D()(x)
-    filters = 2 * filters
+    filters *= 2
 
-# Right branch of Y network
-inputs2 = Input(shape=input_shape)
-y = inputs2
+# right branch of Y network
+right_inputs = Input(shape=input_shape)
+y = right_inputs
 filters = n_filters
+# 3 layers of Conv2D-Dropout-MaxPooling2D
+# number of filters doubles after each layer (32-64-128)
 for i in range(3):
     y = Conv2D(filters=filters,
                kernel_size=kernel_size,
@@ -59,27 +64,34 @@ for i in range(3):
                dilation_rate=2)(y)
     y = Dropout(dropout)(y)
     y = MaxPooling2D()(y)
-    filters = 2 * filters
+    filters *= 2
 
-# Merge left and right branches outputs
-y = keras.layers.concatenate([x, y])
+# merge left and right branches outputs
+y = concatenate([x, y])
+# feature maps to vector in preparation to connecting to Dense layer
 y = Flatten()(y)
 y = Dropout(dropout)(y)
-y = Dense(num_labels)(y)
-outputs = Activation('softmax')(y)
+outputs = Dense(num_labels, activation='softmax')(y)
 
-model = Model([inputs1, inputs2], outputs)
+# build the model in functional API
+model = Model([left_inputs, right_inputs], outputs)
+# verify the model using graph
 plot_model(model, to_file='cnn-y-network.png', show_shapes=True)
+# verify the model using layer text description
 model.summary()
 
+# classifier loss, Adam optimizer, classifier accuracy
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+
+# train the model with input images and labels
 model.fit([x_train, x_train],
           y_train, 
           validation_data=([x_test, x_test], y_test),
           epochs=20,
           batch_size=batch_size)
 
+# model accuracy on test dataset
 score = model.evaluate([x_test, x_test], y_test, batch_size=batch_size)
 print("\nTest accuracy: %.1f%%" % (100.0 * score[1]))
