9b5dad934b4eb7179b8a2ec41e1cc414729ae1c9
Rowel Atienza <rowel.atienza@gmail.com>
2018-07-18 00:00:27 +0800

    weights

--- chapter10-policy/policygradient-car-10.1.1.py
+++ chapter10-policy/policygradient-car-10.1.1.py
@@ -38,8 +38,6 @@ class PolicyAgent():
         n_inputs = env.observation_space.shape[0]
         self.actor_model, self.logprob_model, self.entropy_model, self.value_model = self.build_models(n_inputs)
         loss = self.logprob_loss(self.get_entropy(self.state))
-        if args.actor_critic:
-            self.gamma = 0.99
         self.logprob_model.compile(loss=loss, optimizer=Adam(lr=1e-3))
         self.value_model.compile(loss=self.value_loss, optimizer=Adam(lr=1e-6, clipvalue=0.5))
 
