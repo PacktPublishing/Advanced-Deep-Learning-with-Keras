04c0710bfc760063041207b13fc8e3591223b521
Rowel Atienza <rowel.atienza@gmail.com>
2020-01-02 20:52:22 +0800

    static graph

--- chapter13-mi-unsupervised/iic-13.5.1.py
+++ chapter13-mi-unsupervised/iic-13.5.1.py
@@ -7,8 +7,11 @@ from __future__ import division
 from __future__ import print_function
 
 
+from tensorflow.keras.layers import Input, Dense, Flatten
+from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.utils import plot_model
 from tensorflow.keras import backend as K
 from tensorflow.keras.datasets import mnist
 
@@ -18,44 +21,104 @@ import argparse
 import vgg
 
 from data_generator import DataGenerator
-from utils import unsupervised_labels, center_crop, AccuracyCallback, lr_schedule
-
-from loss import iic_mi_loss
-from vgg import VGGBackbone
-from models import IICEncoder
+from utils import unsupervised_labels, center_crop
+from utils import AccuracyCallback, lr_schedule
 
 
 class IIC:
-    def __init__(self, args):
+    def __init__(self,
+                 args,
+                 backbone):
+        """Contains the encoder model, the loss function,
+            loading of datasets, train and evaluation routine
+            to implement unsupervised clustering via mutual
+            information maximization
+
+        Arguments:
+            args : Command line arguments to indicate choice
+                of batch size, number of heads, folder to save
+                weights file, weights file name, etc
+        """
         self.args = args
+        self.backbone = backbone
+        self._model = None
         self.train_gen = DataGenerator(args, siamese=True)
         self.n_labels = self.train_gen.n_labels
-        self._encoder = IICEncoder(backbone=VGGBackbone(),
-                                   n_heads=args.heads,
-                                   n_labels=self.n_labels)
-        self.init_encoder()
+        self.build_model()
         self.load_eval_dataset()
         self.accuracy = 0
 
-
-    def init_encoder(self):
-        # inputs = Input(shape=self.train_gen.input_shape, name='x')
+    def build_model(self):
+        """Build the n_heads of the IIC model
+        """
+        inputs = Input(shape=self.train_gen.input_shape, name='x')
+        x = self.backbone(inputs)
+        x = Flatten()(x)
+        # number of output heads
+        outputs = []
+        for i in range(self.args.heads):
+            name = "z_head%d" % i
+            outputs.append(Dense(self.n_labels,
+                                 activation='softmax',
+                                 name=name)(x))
+        self._model = Model(inputs, outputs, name='encoder')
         optimizer = Adam(lr=1e-3)
-        loss = iic_mi_loss(self.args.batch_size, self.args.heads)
-        self._encoder.compile(optimizer=optimizer, loss=loss)
+        self._model.compile(optimizer=optimizer, loss=self.mi_loss)
+        self._model.summary()
+
+    def mi_loss(self, y_true, y_pred):
+        """Mutual information loss computed from the joint
+           distribution matrix and the marginals
+
+        Arguments:
+            y_true (tensor): Not used since this is
+                unsupervised learning
+            y_pred (tensor): stack of prediction for
+                the Siamese latent vectors (Z and Zbar)
+        """
+        size = self.args.batch_size
+        n_labels = y_pred.shape[-1]
+        # lower half is Z
+        Z = y_pred[0: size, :]
+        Z = K.expand_dims(Z, axis=2)
+        # upper half is Zbar
+        Zbar = y_pred[size: y_pred.shape[0], :]
+        Zbar = K.expand_dims(Zbar, axis=1)
+        # compute joint distribution
+        P = K.batch_dot(Z, Zbar)
+        P = K.sum(P, axis=0)
+        # enforce symmetric joint distribution
+        P = (P + K.transpose(P)) / 2.0
+        P = P / K.sum(P)
+        # marginal distributions
+        Pi = K.expand_dims(K.sum(P, axis=1), axis=1)
+        Pj = K.expand_dims(K.sum(P, axis=0), axis=0)
+        Pi = K.repeat_elements(Pi, rep=n_labels, axis=1)
+        Pj = K.repeat_elements(Pj, rep=n_labels, axis=0)
+        P = K.clip(P, K.epsilon(), np.finfo(float).max)
+        Pi = K.clip(Pi, K.epsilon(), np.finfo(float).max)
+        Pj = K.clip(Pj, K.epsilon(), np.finfo(float).max)
+        # negative MI loss
+        neg_mi = K.sum((P * (K.log(Pi) + K.log(Pj) - K.log(P))))
+        # each head contribute 1/n_heads to the total loss
+        return neg_mi/self.args.heads
 
 
-    # train the model
     def train(self):
+        """Train function uses the data generator,
+            accuracy computation and learning rate
+            scheduler callbacks
+        """
         accuracy = AccuracyCallback(self)
-        lr_scheduler = LearningRateScheduler(lr_schedule, verbose=1)
+        lr_scheduler = LearningRateScheduler(lr_schedule,
+                                             verbose=1)
         callbacks = [accuracy, lr_scheduler]
-        self._encoder.fit_generator(generator=self.train_gen,
-                                    use_multiprocessing=True,
-                                    epochs=self.args.epochs,
-                                    callbacks=callbacks,
-                                    workers=4,
-                                    shuffle=True)
+        self._model.fit_generator(generator=self.train_gen,
+                                  use_multiprocessing=True,
+                                  epochs=self.args.epochs,
+                                  callbacks=callbacks,
+                                  workers=4,
+                                  shuffle=True)
 
 
     # pre-load test data for evaluation
@@ -81,12 +144,12 @@ class IIC:
             os.makedirs(folder, exist_ok=True) 
             path = os.path.join(folder, self.args.restore_weights)
             print("Loading weights... ", path)
-            self._encoder.load_weights(path)
+            self._model.load_weights(path)
 
 
     # evaluate the accuracy of the current model weights
     def eval(self):
-        y_pred = self._encoder.predict(self.x_test)
+        y_pred = self._model.predict(self.x_test)
         print("")
         # accuracy per head
         for head in range(self.args.heads):
@@ -116,12 +179,12 @@ class IIC:
                 os.makedirs(folder, exist_ok=True) 
                 path = os.path.join(folder, self.args.save_weights)
                 print("Saving weights... ", path)
-                self._encoder.save_weights(path)
+                self._model.save_weights(path)
 
 
     @property
-    def encoder(self):
-        return self._encoder
+    def model(self):
+        return self._model
 
 
 if __name__ == '__main__':
@@ -132,7 +195,7 @@ if __name__ == '__main__':
                        help='Folder for storing model weights (h5)')
     parser.add_argument('--save-weights',
                        default=None,
-                       help='File name for storing model weights (h5)')
+                       help='Folder for storing model weights (h5)')
     parser.add_argument('--dataset',
                        default=mnist,
                        help='Dataset to use')
@@ -157,12 +220,11 @@ if __name__ == '__main__':
                         help='Train the model')
     parser.add_argument('--restore-weights',
                         default=None,
-                        help='File name to restore saved model weights')
+                        help='Restore saved model weights')
     parser.add_argument('--eval',
                         default=False,
                         action='store_true',
-                        help='Evaluate a pre trained model.\
-                              Must indicate weights file.')
+                        help='Evaluate a pre trained model. Must indicate weights file.')
     parser.add_argument('--crop',
                         type=int,
                         default=4,
@@ -174,8 +236,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # build backbone
+    backbone = vgg.VGG(vgg.cfg['F'])
+    backbone.model.summary()
     # instantiate IIC object
-    iic = IIC(args)
+    iic = IIC(args, backbone.model)
     if args.plot_model:
         plot_model(backbone.model,
                    to_file="model-vgg.png",
--- chapter13-mi-unsupervised/vgg.py
+++ chapter13-mi-unsupervised/vgg.py
@@ -6,12 +6,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import BatchNormalization, Activation
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Layer
+
+from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from tensorflow.keras.layers import MaxPooling2D, Input, Flatten, AveragePooling2D
 from tensorflow.keras.models import Model
+
+import numpy as np
 
 # A to E are standard VGG backbones
 # F was customized for IIC
@@ -25,71 +25,44 @@ cfg = {
     'G': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'A'],
 }
 
-class VGGBlock(Layer):
-    def __init__(self, 
-                 n_filters,
-                 name,
-                 maxpool=True,
-                 **kwargs):
-        """A VGG block is made of Conv2D-BN-ReLU.
-        Maxpooling2D is optional at the last layer.
+# VGG backbone class
+class VGG():
+    def __init__(self, cfg, input_shape=(24, 24, 1)):
+        self.cfg = cfg
+        self.input_shape = input_shape
+        self._model = None
+        self.build_model()
 
-        Arguments:
-            n_filters (int): Number of Conv2D filters
-            name (string): Block name
-            maxpool (Bool): Use MaxPoolinng2D as the
-                last layer.
-        """
-        super(VGGBlock, self).__init__(name=name, **kwargs)
-        self.conv = Conv2D(n_filters,
-                           kernel_size=3,
-                           padding='same',
-                           kernel_initializer='he_normal')
-        self.bn = BatchNormalization()
-        self.relu = Activation('relu')
-        if maxpool:
-            self.maxp = MaxPooling2D()
+    def build_model(self):
+        inputs = Input(shape=self.input_shape, name='x')
+        x = make_layers(self.cfg, inputs)
+        self._model = Model(inputs, x, name='VGG')
+
+    @property
+    def model(self):
+        return self._model
+
+# helper function to interpret config and make layers from it
+def make_layers(cfg, inputs, batch_norm=True, in_channels=1):
+    x = inputs
+    for v in cfg:
+        if v == 'M':
+            x = MaxPooling2D()(x)
+        elif v == 'A':
+            x = AveragePooling2D(pool_size=3)(x)
         else:
-            self.maxp = None
-
-
-    def call(self, inputs):
-        x = self.conv(inputs)
-        x = self.bn(x)
-        x = self.relu(x)
-        if self.maxp is not None:
-            x = self.maxp(x)
-        return x
+            x = Conv2D(v,
+                       kernel_size=3,
+                       padding='same',
+                       kernel_initializer='he_normal'
+                       )(x)
+            if batch_norm:
+                x = BatchNormalization()(x)
+            x = Activation('relu')(x)
     
+    return x
 
-class VGGBackbone(Model):
-    def __init__(self, 
-                 name='VGG_backbone', 
-                 **kwargs):
-        """VGG backbone object creator.
-        This is a simplified VGG made of 4 conv for
-        feature extraction.
-            
-        Arguments
-            name (string): Layer name
-        """
-        super(VGGBackbone, self).__init__(name=name, **kwargs)
-        self.block1 = VGGBlock(64 , "VGG_block1")
-        self.block2 = VGGBlock(128, "VGG_block2")
-        self.block3 = VGGBlock(256, "VGG_block3")
-        self.block4 = VGGBlock(512, "VGG_block4", maxpool=False)
-
-
-    def call(self, inputs):
-        x = self.block1(inputs)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        return x
-
-
+        
 if __name__ == '__main__':
-    backbone = VGGBackbone()
-    y = backbone(tf.ones(shape=(1, 24, 24, 1)))
-    backbone.summary()
-    #print(len(backbone.weights))
+    backbone = VGG(cfg['F'])
+    backbone.model.summary()
